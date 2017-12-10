cd072475f34caa07cedf20de9aa98ee3d2601f20
Alexander <BlackPyro94@gmail.com>
2017-12-11 00:09:55 +0100

    Was noch ""nicht""  funktioniert ::
    Links Liste korrekt Aufklappen;
    Plan laden
    Plan speichern
    PDF
    Module Verschieben
    (...)
    ---------------------------------
    Was klappt ::
    
    Module aus CSV laden und links auflisten
    Module aus Sem löschen
    Plan erstellen
    Plus Minus Button

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,13 @@
 package controllers;
 
+import play.api.libs.json.Json;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -18,10 +23,23 @@ public class HomeController extends Controller {
      */
     public Result index() {
 
+        ArrayList<Object[]> modulListe = new ArrayList<Object[]>();
+
+        modulListe.add(new Object[]{7,"Modul Alpha"});
+        modulListe.add(new Object[]{8,"Modul Beta"});
+        modulListe.add(new Object[]{9,"Modul Gamma"});
+        modulListe.add(new Object[]{10,"Modul Delta"});
+        modulListe.add(new Object[]{11,"Modul Epsilon"});
+
         // String[] userAgent = request().headers().get(Http.HeaderNames.USER_AGENT);
+
         String userAgent = request().getHeader("User-Agent");
+
+        String[] arr = new String[]{"abc", "abc2"};
+
         // return ok(index.render("asdasd"));
-        return ok(index.render(userAgent));
+
+        return ok(index.render(userAgent, modulListe));
     }
 
 }
