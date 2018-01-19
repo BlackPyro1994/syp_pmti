1a5890800023c4ad26f32cde85b6934026963051
Alexander <BlackPyro94@gmail.com>
2018-01-19 13:42:42 +0100

    Added : Click_Selector() Methode

---
+++ public/Desktop/Functions/click_selector.js
@@ -0,0 +1,29 @@
+function click_selector()
+{
+    var selector = $(this)
+        .parents()
+        .map(function ()
+        {
+            return this.tagName;
+        })
+        .get()
+        .reverse()
+        .concat([this.nodeName])
+        .join(">");
+    
+    var id = $(this).attr("id");
+    if (id)
+    {
+        selector += "#" + id;
+    }
+    
+    var classNames = $(this).attr("class");
+    if (classNames)
+    {
+        selector += "." + $.trim(classNames).replace(/\s/gi, ".");
+    }
+    
+    // alert(selector);
+    console.log(selector);
+    return false;
+}
