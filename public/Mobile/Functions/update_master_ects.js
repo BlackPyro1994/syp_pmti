0e72b7d9e445932ec4a958f47fc49bf9a55127d4
Alexander <BlackPyro94@gmail.com>
2018-01-19 04:40:20 +0100

    MOBILE : Master ECTS Update Klappt

--- public/Mobile/Functions/update_master_ects.js
+++ public/Mobile/Functions/update_master_ects.js
@@ -1,27 +1,81 @@
-function update_master_ects(mod_id, wert) {
-    console.log("update_master_ects(" + mod_id + "," + wert + ")");
+/*
+
+function update_master_ects(mod_id, wert)
+{
+    var ects = Number(document.getElementById("master_ects").textContent);
+    // wert = auswahl ob addieren oder subtrahieren
+    // 1 == adieren 0 == subtrahieren
+    if (wert == 1)
+    {
+        @for(mod <- modulListe) {
+        if ("@{mod(0)}" == mod_id)
+        {
+            if (mod_id == "ALM")
+            {
+                document.getElementById("master_ects").textContent = ects;
+            }
+            else
+            {
+                ects = ects + @{mod(9)};
+                document.getElementById("master_ects").textContent = ects;
+            }
+        }
+    }
+    }
+    else if (wert == 0)
+    {
+        @for(mod <- modulListe) {
+        if ("@{mod(0)}" == mod_id)
+        {
+            if (mod_id == "ALM")
+            {
+                document.getElementById("master_ects").textContent = ects;
+            }
+            else
+            {
+                ects = ects - @{mod(9)};
+                document.getElementById("master_ects").textContent = ects;
+            }
+        }
+    }
+    }
+}
+
+*/
+
+function update_master_ects(mod_id, wert)
+{
+    var ects = Number(document.getElementById("master_ects_punkte").textContent);
+    // wert = auswahl ob addieren oder subtrahieren
+    // 1 == adieren 0 == subtrahieren
     
-    // wert = auswahl ob addieren(1) oder subtrahieren(0)
-    if (wert == 1) {
-        @for(mod <- modulListe) {
-            if ("@{mod(0)}" == mod_id) {
-                if (mod_id != "ALM") {
-                    master_ects = master_ects + @{mod(9)};
-                    $("#ects_punkte").text(master_ects);
-                }
-                
-                
-            }
+    updateModuleString(mod_id);
+    
+    modulString = modulString.split(",");
+    
+    if (wert == 1)
+    {
+        if (mod_id == "ALM")
+        {
+            document.getElementById("master_ects_punkte").textContent = ects;
+        }
+        else
+        {
+            ects = Number.parseInt(ects) + Number.parseInt(modulString[9]);
+            document.getElementById("master_ects_punkte").textContent = ects;
         }
     }
-    else if (wert == 0) {
-        @for(mod <- modulListe) {
-            if ("@{mod(0)}" == mod_id) {
-                if (mod_id != "ALM") {
-                    master_ects = master_ects - @{mod(9)};
-                    $("#ects_punkte").text(master_ects);
-                }
-            }
+    else if (wert == 0)
+    {
+        if (mod_id == "ALM")
+        {
+            document.getElementById("master_ects_punkte").textContent = ects;
+        }
+        else
+        {
+            ects = Number.parseInt(ects) - Number.parseInt(modulString[9]);
+            document.getElementById("master_ects_punkte").textContent = ects;
         }
     }
+    
 }
