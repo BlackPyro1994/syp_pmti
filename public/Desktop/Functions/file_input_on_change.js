1575ce19211a4fe8fdcc11708f58432901632c1f
Alexander <BlackPyro94@gmail.com>
2018-01-19 13:29:24 +0100

    Firefox Fehler (OK)

--- public/Desktop/Functions/file_input_on_change.js
+++ public/Desktop/Functions/file_input_on_change.js
@@ -1,43 +1,46 @@
-document.getElementById('file-input').onchange = function ()
+$(document).ready(function ()
 {
-    var file = this.files[0];
-    var reader = new FileReader();
-    reader.onload = function (progressEvent)
+    document.getElementById('file-input').onchange = function ()
     {
-        var temp;
-        var lines = this.result.split('\n');
-        temp = lines[0].split(',');
-        ok_button_new_plan({"count": temp[1], "semStart": temp[0]});
-        for (var line = 1; line < lines.length; line++)
+        var file = this.files[0];
+        var reader = new FileReader();
+        reader.onload = function (progressEvent)
         {
-            temp = lines[line];
-            if (temp.split(',')[1] === "M07" || temp.split(',')[1] === "ALM")
+            var temp;
+            var lines = this.result.split('\n');
+            temp = lines[0].split(',');
+            ok_button_new_plan({"count": temp[1], "semStart": temp[0]});
+            for (var line = 1; line < lines.length; line++)
             {
-                // temp = temp.substring(1);
-                temp = temp.split(',');
-                insert_module_in_semester(temp[1], temp[0], {
-                    "mod_id": temp[1],
-                    "sem_id": temp[0],
-                    "name": temp[2],
-                    "v": temp[3],
-                    "u": temp[4],
-                    "p": temp[5],
-                    "pr": temp[6],
-                    "dozent": temp[7],
-                    "ects": temp[8]
-                });
-            }
-            else
-            {
-                temp = temp.split(',');
-                insert_module_in_semester(temp[1], temp[0]);
+                temp = lines[line];
+                if (temp.split(',')[1] === "M07" || temp.split(',')[1] === "ALM")
+                {
+                    // temp = temp.substring(1);
+                    temp = temp.split(',');
+                    insert_module_in_semester(temp[1], temp[0], {
+                        "mod_id": temp[1],
+                        "sem_id": temp[0],
+                        "name": temp[2],
+                        "v": temp[3],
+                        "u": temp[4],
+                        "p": temp[5],
+                        "pr": temp[6],
+                        "dozent": temp[7],
+                        "ects": temp[8]
+                    });
+                }
+                else
+                {
+                    temp = temp.split(',');
+                    insert_module_in_semester(temp[1], temp[0]);
 //				  for (var pos = 1; pos < temp.length; pos++)
 //				  {
 // 				     insert_module_in_semester(temp[pos], temp[0]);
 //				  }
+                }
             }
-        }
-        document.getElementById('file-input').value = "";
-    };
-    reader.readAsText(file);
-}
+            document.getElementById('file-input').value = "";
+        };
+        reader.readAsText(file);
+    }
+})
