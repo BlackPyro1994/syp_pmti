0bba68eb6e998ca66c2f241f797c24efd908b836
Alexander <BlackPyro94@gmail.com>
2018-01-19 16:24:26 +0100

    remove_module_from_content

---
+++ public/Desktop/Functions/remove_module_from_content.js
@@ -0,0 +1,60 @@
+function remove_module_from_content(module_id)
+{
+    
+    console.log(content);
+    console.log("-------------------------------------------------------------------");
+    
+    var BreakException = {};
+    
+    try
+    {
+        
+        for (sem = 0; sem < content.length; sem++)
+        {
+            for (mod = 0; mod < content[sem].length; mod++)
+            {
+                var string = (content[sem][mod]);
+                
+                string = string.substr(1, string.length);
+                string = string.split(",");
+                
+                // console.log(string);
+                
+                // console.log("Semester : " + sem + " Modul : " + string);
+                
+                // console.log(string[0] + " == " + module_id + " ?");
+                
+                if (string[0] == module_id)
+                {
+                    console.log("Modul " + module_id + " wird entfernt !");
+                    // content[sem] = content[sem].splice(mod,1);
+                    
+                    var newArray = [];
+                    
+                    for (i = 0; i < content[sem].length; i++)
+                    {
+                        if (content[i] != mod)
+                        {
+                            newArray.push(content[i]);
+                            content[sem] = newArray;
+                            
+                            console.log("-------------------------------------------------------------------");
+                            console.log(content);
+                            
+                            throw BreakException;
+                        }
+                    }
+                    
+                    
+                }
+            }
+        }
+        
+    }
+    
+    catch (e)
+    {
+        if (e !== BreakException) throw e;
+    }
+    
+}
