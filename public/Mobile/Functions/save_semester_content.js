4245493c98e74b6faf09869e40ffd758a3d6da04
Alexander <blackpyro94@gmail.com>
2018-01-19 09:33:19 +0100

    Mobile - Button "Übersicht" überflüssig.

--- public/Mobile/Functions/save_semester_content.js
+++ public/Mobile/Functions/save_semester_content.js
@@ -1,4 +1,4 @@
 function save_semester_content(global_sem_id) {
     console.log("save_semester_content(" + global_sem_id + ")");
-    semester_array[global_sem_id] = $("#div_semester").children();
+    content[global_sem_id] = $("#div_semester").children();
 }
--- public/Mobile/Functions/show_semester_content.js
+++ public/Mobile/Functions/show_semester_content.js
@@ -1,4 +1,4 @@
 function show_semester_content(global_sem_id) {
     console.log("show_semester_content(" + global_sem_id + ")");
-    $("#div_semester").append(semester_array[global_sem_id]);
+    $("#div_semester").append(content[global_sem_id]);
 }
