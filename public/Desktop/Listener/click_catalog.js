66283032d43957e4131c3e55c12dea36a1772617
Alexander <BlackPyro94@gmail.com>
2018-01-19 22:12:43 +0100

    Desktop --> click_catalog.js

--- public/Desktop/Listener/click_catalog.js
+++ public/Desktop/Listener/click_catalog.js
@@ -1,46 +1,21 @@
 function click_catalog(click)
 {
-    console.log("-----------------------------------------------------------");
-    
-    console.log("open_catalog : " + "(" + open_catalog + ")");
-    
-    console.log("$(click).siblings().is(':hidden') : " + "(" + $(click).siblings().is(':hidden') + ")");
-    
-    if (open_catalog != "")
-    {
-        console.log("$(#" + open_catalog + ").siblings().is(':visible') : " + $("#" + open_catalog).siblings().is(':visible'));
-    }
-    
-    console.log("(" + open_catalog + ") == (" + $(click).attr("id") + ") ? ");
-    
-    blocked = false;
-    
     if (!blocked)
     {
         if (all_catalogs_closed == 0)
         {
-            // ################################################################################################
-            //              Neuer Katalog
-            // ################################################################################################
-            
             if (($(click).siblings().is(':hidden')) && open_catalog == "")
             {
                 $(click).siblings().toggle(200);
                 open_catalog = $(click).attr("id");
             }
             
-            // ################################################################################################
-            //              Gleiche Katalog (schließen)
-            // ################################################################################################
             else if ($(click).attr("id") == open_catalog)
             {
                 $(click).siblings().toggle(200);
                 open_catalog = "";
             }
             
-            // ################################################################################################
-            //              Wechsel von Katalogen
-            // ################################################################################################
             else if (( $("#" + open_catalog).siblings().is(':visible')) && open_catalog != $(click).attr("id"))
             {
                 $("#" + open_catalog).siblings().toggle(200);
@@ -49,98 +24,5 @@ function click_catalog(click)
             }
         }
     }
-}
-
-// ###########################################################################
-function DEACTIVATED_click_catalog(click)
-{
-    if (!blocked)
-    {
-        click.preventDefault();
-        click = click.target;
-        $(click).closest(".class_click_catalog").css("background", "#eee");
-        
-        if ($(click).attr('class').includes("class_click_catalog"))
-        {
-            
-            if (all_catalogs_closed == 0)
-            {
-                
-                // ################################################################################################
-                //              Neuer Katalog
-                // ################################################################################################
-                
-                if (($(click).parent().siblings().is(':hidden')) && open_catalog == "")
-                {
-                    var new_click = $(click);
-                    
-                    $(new_click).parent().siblings().toggle(200);
-                    
-                    // ### open_catalog = $(".modul:visible").closest(".class_click_catalog").attr("id");
-                    
-                    // $(click).parent().parent().toggleClass("open_catalog");
-                    
-                    // ### $(click).closest(".class_click_catalog").toggleClass("open_catalog");
-                    
-                }
-                
-                // ################################################################################################
-                //              Gleiche Katalog (schließen)
-                // ################################################################################################
-                
-                else if ($(click).closest(".class_click_catalog").attr("id") == open_catalog)
-                {
-                    
-                    // ### $(click).closest(".class_click_catalog").toggleClass("open_catalog");
-                    
-                    // ###  var new_click = $(click);
-                    
-                    // ### $(new_click).parent().siblings().not('.class_click_catalog').not('.class_click_catalog_label').toggle(200);
-                    
-                    // ###  open_catalog = "";
-                    
-                    // $(click).parent().parent().toggleClass("open_catalog");
-                    
-                    // $(click).closest(".class_click_catalog").toggleClass("open_catalog");
-                    
-                }
-                
-                // ################################################################################################
-                //              Wechsel von Katalogen
-                // ################################################################################################
-                
-                else if (( $("#" + open_catalog).children().first().siblings().is(':visible')) && open_catalog != $(click).closest(".class_click_catalog").attr("id"))
-                {
-                    
-                    $("#" + open_catalog).children().first().siblings().toggle("hidden");
-                    
-                    $("#" + open_catalog).removeClass("open_catalog");
-                    
-                    // $(click).parent().parent().toggleClass("open_catalog");
-                    
-                    $(click).closest(".class_click_catalog").toggleClass("open_catalog");
-                    
-                    var new_click = $(click);
-                    
-                    $(new_click).parent().siblings().show(200);
-                    
-                    open_catalog = $(click).parent().parent().attr("id");
-                    
-                }
-                
-            }
-            
-        }
-        else if ($(click).attr('class').includes("class_click_module_p"))
-        {
-            select_module(click);
-        }
-        else if ($(click).attr('class').includes("class_click_module_row"))
-        {
-            return;
-        }
-        
-    }
-    
 }
 
