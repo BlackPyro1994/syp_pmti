3fe7c48a68634f7b97c6a8f0e2dab80ba7ece8d9
Alexander <BlackPyro94@gmail.com>
2018-01-19 17:35:57 +0100

    remove_module_from_content(modul id) OPTIMIERT !

--- public/Desktop/Functions/remove_module_from_content.js
+++ public/Desktop/Functions/remove_module_from_content.js
@@ -1,67 +1,37 @@
 function remove_module_from_content(module_id)
 {
-    
-    //console.log(content);
-    console.log("-------------------------------------------------------------------");
-    
     var BreakException = {};
+    
+    var position_x_y = [];
+    
+    var newArray = [];
     
     try
     {
-        
-        for (sem = 0; sem < content.length; sem++)
+        for (sem_index = 0; sem_index < content.length; sem_index++)
         {
-            for (mod = 0; mod < content[sem].length; mod++)
+            for (mod_index = 0; mod_index < content[sem].length; mod_index++)
             {
-                var string = (content[sem][mod]);
-                
-               // string = string.substr(1, string.length);
-                string = string.split(",");
-                
-                // console.log(string);
-                
-                // console.log("Semester : " + sem + " Modul : " + string);
-                
-                // console.log(string[0] + " == " + module_id + " ?");
-                
-                if (string[1] == module_id)
+                if ((content[sem][mod]).split(",")[1] == module_id)
                 {
-                    console.log("Modul " + module_id + " wird entfernt !");
-                    // content[sem] = content[sem].splice(mod,1);
-                    
-                    var newArray = [];
-                    
-                    for (i = 0; i < content[sem].length; i++)
-                    {
-                        console.log(content[sem][i]);
-                       // if (content[sem][i] != mod)
-                        if (i!=mod)
-
-                        {
-                            newArray.push(content[sem][i]);
-
-                            
-                            console.log("-------------------------------------------------------------------");
-                            //console.log(newArray);
-                            //console.log(content);
-                            
-                          //  throw BreakException;
-                        }
-
-                    }
-                    content[sem] = newArray;
-                    console.log(content);
-                    
-                    
+                    position_x_y = [sem, mod];
+                    throw BreakException;
                 }
             }
         }
-        
     }
-    
-    catch (e)
+    catch (BreakException)
     {
-        if (e !== BreakException) throw e;
+        for (i = 0; i < content[sem].length; i++)
+        {
+            if (i != mod)
+            {
+                newArray.push(content[sem][i]);
+            }
+            
+        }
+        
+        content[sem] = newArray;
     }
     
 }
