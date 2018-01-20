9ce12a0e5e2a6ea7746953a18028f0b99e9c52c5
GutHibM <unverzogen-1@yahoo.de>
2018-01-20 21:13:59 +0100

    create_pdf auf mobiler

--- public/Mobile/Functions/create_pdf.js
+++ public/Mobile/Functions/create_pdf.js
@@ -40,20 +40,10 @@ function create_pdf() {
     y += 10;
 
 
-    //var anz_Sem = Number(document.getElementById("count").textContent);
-
     var semester = startSem == "winter" ? "Wintersemester" : "Sommersemester";
 
 
     for (i = 1; i <= count; i++) {
-
-        //var anz_Module = $("#div_semester").children().filter(":nth-child(" + i + ")").children().length - 1;
-        //console.log("content: "+content);
-        //console.log("catalog_array: "+catalog_array);
-        //console.log("blocked: "+blocked);
-
-
-        //console.log(modulListe);
 
 
         var anz_Module = catalog_array.length;
@@ -66,7 +56,7 @@ function create_pdf() {
         }
         restplatz -= ((anz_Module * 5) + 12);
 
-        //ab hier 1 hart geschriebene Werte zu tauschen:
+ //ab hier 1 hart geschriebene Werte zu tauschen:
 
         sem_ects = "30";
 
@@ -109,43 +99,19 @@ function create_pdf() {
             x = 11;
 
 
-            // Semester i beginnt mit 1
-
-            // content[i-1].forEach(function (entry)
-            //console.log("Content: " + content);
-            //console.log("Content i-1 : " + content[i - 1]);
-//j < content[i-1].length
-
             for (j = 0; j<content[i-1].length; j++) {
-                console.log("i: "+i);
-                console.log("Content i-1 : " + content[i - 1]);
-                console.log("content[i-1].length " +content[i-1].length);
-                console.log("FOR : " + "(" + j + ")");
-               // console.log("Semester : (" + j + ") Modul : (" + (content[i - 1])[j] + ")");
-                //console.log("content[i - 1])[j].length: " + content.length);
-                console.log("content[i-1].length: " + content[i-1].length);
-                console.log(content[i - 1][j]);
+
                 liste = (content[i - 1][j]) ;
 
-               // console.log("Liste : " + liste);
-                console.log(liste);
 
                 id = liste[0];
-                console.log("id: "+id);
                 name = liste[1];
-                console.log("name: "+name);
                 v = liste[2];
-                console.log("v: "+v);
                 u = liste[3];
-                console.log("u: "+u);
                 p = liste[4];
-                console.log("p: "+p);
                 pr = liste[5];
-                console.log("pr: "+pr);
                 dozent = liste[8];
-                console.log("dozent: "+dozent);
                 ects = liste[9];
-                console.log("ects: "+ects);
 
                 doc.setFont("courier");
                 doc.setFontSize(10);
