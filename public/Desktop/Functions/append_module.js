b50b0b578510b30cf8587a16aff2264fae3a45db
PeBoddy <peboddy@users.noreply.github.com>
2018-01-20 14:26:04 +0100

    desktop gui -> Modulansicht im Semester repariert -> delete Button SCHROTT

--- public/Desktop/Functions/append_module.js
+++ public/Desktop/Functions/append_module.js
@@ -8,29 +8,25 @@
     $("#div_semester").children().filter(":nth-child(" + sem_id + ")").append(
         
         '<div class = "row" > ' +
-        
-            '<div '+'id="mod_' + mod_id + '"' + 'class = "semeter row">' +
-        
-                '<div class = "col-auto">' + '<p >ID: ' + mod_id + '</p>' + '</div>' +
-        
-                '<div class = "col-auto">' + '<p >' + search_result[1] + '</p>' + '</div>' +
-        
-                '<div class = "col-auto">' + '<p >' + 'V:' + search_result[2] + ',Ü:' + search_result[3] + ',P:' + search_result[4] + ',Pr:' + search_result[5] + '</p>' + '</div>' +
-        
-                '<div class = "col-auto">' + '<p >' + search_result[8] + '</p>' + '</div>' +
-        
-                '<div class = "col-auto">' + '<p style="vertical-align: middle,text-align: center">ECTS:' + search_result[9] + '</p>' + '</div>' +
-        
+            '<div '+'id="mod_' + mod_id + '"' + 'class = "col-10">' +
+                '<div class="row semester">' +
+                    '<div class = "col-2 tags">' + '<p >ID: ' + mod_id + '</p>' + '</div>' +
+
+                    '<div class = "col-3 tags">' + '<p >' + search_result[1] + '</p>' + '</div>' +
+
+                    '<div class = "col-3 tags">' + '<p >' + 'V:' + search_result[2] + ',Ü:' + search_result[3] + ',P:' + search_result[4] + ',Pr:' + search_result[5] + '</p>' + '</div>' +
+
+                    '<div class = "col-2 tags">' + '<p >' + search_result[8] + '</p>' + '</div>' +
+
+                    '<div class = "col">' + '<p style="vertical-align: middle,text-align: center">ECTS:' + search_result[9] + '</p>' + '</div>' +
+                '</div>' +
             '</div>' +
-        
-            '<button type = "button" class = "button_delete col-auto btn btn-danger">x</button>' +
-    
-            '<button type = "button" class = "button_move col-auto btn btn-warning">>></button>'
-        
-        + '</div>');
-    
-    
+            '<div class="col">' +
+                '<button id ="'+mod_id+'" type = "button" class = "button_delete col-5 btn btn-danger btn-sm semester absolute_center">x</button>' +
+                '<button id ="'+mod_id+'" type = "button" class = "button_move col-5 btn btn-warning btn-sm semester absolute_center">>></button>' +
+            '</div>' +
+        '</div>');
+
     content[sem_id - 1].push(search_result);
-    
-    
+
 }
