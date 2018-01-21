ffb813ac4267f14a670672ba8ebc1c5f362c438a
PeBoddy <peboddy@users.noreply.github.com>
2018-01-22 00:26:29 +0100

    desktop gui -> semester überrschriften angepasst

--- public/Desktop/Functions/add_semester.js
+++ public/Desktop/Functions/add_semester.js
@@ -10,12 +10,12 @@ function add_semester(startSem)
         
         '<button id="semester_'+ wert +'"class = "button_semester container-fluid semester-gesamt class_click_semester">' +
                 '<div  class = "row">' +
-                        '<h6 id="' + 'sem' + wert + '"class="col">' + wert + '</h6>' +
-                        '<h6 class="col label"></h6>' +
-                        '<h6 class="col sem_ects">ECTS: </h6>' +
-                        '<h6 class="col sem_ects" id="' + 'sem_ects_anz' + wert + '"></h6>' +
+                        '<h6 id="' + 'sem' + wert + '"class="col-1">' + wert + '</h6>' +
+                        '<h6 class="col-3 label text-left"></h6>' +
+                        '<h6 class="col-1 sem_ects">ECTS:</h6>' +
+                        '<h6 class="col-1 sem_ects text-left" id="' + 'sem_ects_anz' + wert + '"></h6>' +
                 '</div>' +
-        '</button>');
+        '</button>')
 
     if (startSem == "winter" && wert % 2 == 1)
     {
