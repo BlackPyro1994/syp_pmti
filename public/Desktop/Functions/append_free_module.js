7370e3c9b663ceb26df5d1710a43465599563d50
PeBoddy <peboddy@users.noreply.github.com>
2018-01-21 20:33:38 +0100

    desktop gui -> alm modul ansicht(im Semester) angepasst

--- public/Desktop/Functions/append_free_module.js
+++ public/Desktop/Functions/append_free_module.js
@@ -29,6 +29,28 @@
 
     content[sem_id - 1].push(mod_id + "," + name + "," + v + "," + u + "," + p + "," + pr + "," + dozent + "," + ects);
 
-    $("#div_semester").children().filter(":nth-child(" + sem_id + ")").append('<div id="mod_' + mod_id + '"' + ' class = "row semester">' + '<div class = "col tags id">' + '<p class="class_click_p">ID: ' + mod_id + '</p>' + '</div>' + '<div class = "col tags name">' + '<p class="class_click_p">' + name + '</p>' + '</div>' + '<div class = "col tags sws">' + '<p class="class_click_p">V:' + v + ',Ü:' + u + ',P:' + p + ',Pr:' + pr + '</p>' + '</div>' + '<div class = "col tags dozent">' + '<p class="class_click_p">' + dozent + '</p>' + '</div>' + '<div class = "col tags ects">' + '<p class="class_click_p" style="vertical-align: middle,text-align: center">ECTS: ' + ects + '</p>' + '</div>' + '<div class = "col-1 tags">' + '<button type = "button" id = "button_loeschen" class = " btn btn-danger btn-sm btn-block class_click_delete">x</button>' + '</div>' + '<div class = "col-1">' + '<button type = "button" id = "button_verschieben" class = "btn btn-warning btn-sm btn-block class_click_move">>></button>' + '</div>' + '</div>');
+    //$("#div_semester").children().filter(":nth-child(" + sem_id + ")").append('<div id="mod_' + mod_id + '"' + ' class = "row semester">' + '<div class = "col tags id">' + '<p class="class_click_p">ID: ' + mod_id + '</p>' + '</div>' + '<div class = "col tags name">' + '<p class="class_click_p">' + name + '</p>' + '</div>' + '<div class = "col tags sws">' + '<p class="class_click_p">V:' + v + ',Ü:' + u + ',P:' + p + ',Pr:' + pr + '</p>' + '</div>' + '<div class = "col tags dozent">' + '<p class="class_click_p">' + dozent + '</p>' + '</div>' + '<div class = "col tags ects">' + '<p class="class_click_p" style="vertical-align: middle,text-align: center">ECTS: ' + ects + '</p>' + '</div>' + '<div class = "col-1 tags">' + '<button type = "button" id = "button_loeschen" class = " btn btn-danger btn-sm btn-block class_click_delete">x</button>' + '</div>' + '<div class = "col-1">' + '<button type = "button" id = "button_verschieben" class = "btn btn-warning btn-sm btn-block class_click_move">>></button>' + '</div>' + '</div>');
 
+    var filter_string = ":nth-child(" + sem_id + ")";
+    $("#div_semester").children().filter(filter_string).append(
+
+        '<div class = "row" > ' +
+        '<div ' + 'id="mod_' + mod_id + '"' + 'class = "col-10">' +
+        '<div class="row semester">' +
+        '<div class = "col-2 tags">' + '<p >ID: ' + mod_id + '</p>' + '</div>' +
+
+        '<div class = "col-3 tags">' + '<p >' + name + '</p>' + '</div>' +
+
+        '<div class = "col-3 tags">' + '<p >' + 'V:' + v + ',Ü:' + u + ',P:' + p + ',Pr:' + pr + '</p>' + '</div>' +
+
+        '<div class = "col-2 tags">' + '<p >' + dozent + '</p>' + '</div>' +
+
+        '<div class = "col">' + '<p style="vertical-align: middle,text-align: center">ECTS:' + ects + '</p>' + '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col">' +
+        '<button id ="' + mod_id + '" type = "button" class = "button_delete col-5 btn btn-danger btn-sm semester absolute_center">x</button>' +
+        '<button id ="' + mod_id + '" type = "button" class = "button_move col-5 btn btn-warning btn-sm semester absolute_center">>></button>' +
+        '</div>' +
+        '</div>');
 }
