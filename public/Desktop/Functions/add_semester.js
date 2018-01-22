fb21dc7a0ed3751d98f5006831084529ce2444f2
PeBoddy <peboddy@users.noreply.github.com>
2018-01-22 10:07:59 +0100

    mobile gui -> counter_minus() repariert

--- public/Desktop/Functions/add_semester.js
+++ public/Desktop/Functions/add_semester.js
@@ -1,41 +1,35 @@
-function add_semester(startSem)
-{
+function add_semester(startSem) {
     content.push(new Array());
 
     var wert = Number(document.getElementById("count").textContent);
 
     var anzahl = content.length;
-    
+
     $("#div_semester").append(
-        
-        '<button id="semester_'+ wert +'"class = "button_semester container-fluid semester-gesamt class_click_semester">' +
-                '<div  class = "row">' +
-                        '<h6 id="' + 'sem' + wert + '"class="col-1">' + wert + '</h6>' +
-                        '<h6 class="col-3 label text-left"></h6>' +
-                        '<h6 class="col-1 sem_ects">ECTS:</h6>' +
-                        '<h6 class="col-1 sem_ects text-left" id="' + 'sem_ects_anz' + wert + '"></h6>' +
-                '</div>' +
+        '<button id="semester_' + wert + '"class = "button_semester container-fluid semester-gesamt class_click_semester">' +
+        '<div  class = "row">' +
+        '<h6 id="' + 'sem' + wert + '"class="col-1">' + wert + '</h6>' +
+        '<h6 class="col-3 label text-left"></h6>' +
+        '<h6 class="col-1 sem_ects">ECTS:</h6>' +
+        '<h6 class="col-1 sem_ects text-left" id="' + 'sem_ects_anz' + wert + '"></h6>' +
+        '</div>' +
         '</button>')
 
-    if (startSem == "winter" && wert % 2 == 1)
-    {
+    if (startSem == "winter" && wert % 2 == 1) {
         $(".label").last().text("Winter-Semester");
         $(".sem_ects").last().text("0");
     }
-    else if (startSem == "winter" && wert % 2 == 0)
-    {
+    else if (startSem == "winter" && wert % 2 == 0) {
         $(".label").last().text("Sommer-Semester");
         $(".sem_ects").last().text("0");
     }
-    else if (startSem == "sommer" && wert % 2 == 1)
-    {
+    else if (startSem == "sommer" && wert % 2 == 1) {
         $(".label").last().text("Sommer-Semester");
         $(".sem_ects").last().text("0");
     }
-    else
-    {
+    else {
         $(".label").last().text("Winter-Semester");
         $(".sem_ects").last().text("0");
     }
-    
+
 }
--- public/Mobile/Functions/counter_minus.js
+++ public/Mobile/Functions/counter_minus.js
@@ -9,65 +9,67 @@ function counter_minus() {
     var sem_id = $("#div_semester").children().last().attr('id');
     if (count > 0) {
         count--;
-    }
-    $("#div_semester").children().last().remove();
-    console.log("LÖSCHE SEMESTER: " + sem_id);
 
-    sem_id = sem_id.replace("sem", "");
-    console.log("");
-    console.log("Vor der Schleife:");
-    console.log("content: " + content);
-    console.log("blocked: " + blocked);
-    console.log("catalog_array: " + catalog_array);
+        
+        $("#div_semester").children().last().remove();
+        console.log("LÖSCHE SEMESTER: " + sem_id);
 
-
-    for (i = 0; i < content[sem_id - 1].length; i++) {
+        sem_id = sem_id.replace("sem", "");
         console.log("");
-        console.log("content[sem_id-1].length: " + content[sem_id - 1].length);
-        console.log("schleife i: " + i);
+        console.log("Vor der Schleife:");
+        console.log("content: " + content);
+        console.log("blocked: " + blocked);
+        console.log("catalog_array: " + catalog_array);
 
 
-        mod_id = content[sem_id - 1][i][0];
-        cat_id = content[sem_id - 1][i][11];
-        console.log("mod_id: " + mod_id);
+        for (i = 0; i < content[sem_id - 1].length; i++) {
+            console.log("");
+            console.log("content[sem_id-1].length: " + content[sem_id - 1].length);
+            console.log("schleife i: " + i);
 
-        console.log("cat_id: " + cat_id);
+
+            mod_id = content[sem_id - 1][i][0];
+            cat_id = content[sem_id - 1][i][11];
+            console.log("mod_id: " + mod_id);
+
+            console.log("cat_id: " + cat_id);
 
 
-        console.log("content.indexOf(mod_id): " + content.indexOf(mod_id));
+            console.log("content.indexOf(mod_id): " + content.indexOf(mod_id));
 
-        index = blocked.indexOf(mod_id);
-        if (index != -1) {
-            blocked.splice(index, 1);
+            index = blocked.indexOf(mod_id);
+            if (index != -1) {
+                blocked.splice(index, 1);
+            }
+
+            index = catalog_array.indexOf(cat_id);
+            if (index != -1) {
+                catalog_array.splice(index, 1);
+            }
+
+            /*
+            index = content[sem_id - 1][i].indexOf(mod_id);
+            if (index != -1) {
+                content[sem_id - 1].splice(index, 1);
+                content[sem_id - 1][i].splice(index, 12);
+                content[sem_id - 1] = newArray;
+            }
+            */
+            remove_module_from_content(mod_id);
+            update_master_ects(mod_id);
+
+            //FEHLER
+            //Springt aus Schleife nach Aufruf
+            //Content wird nicht zurückgesetzt
+            //remove_module_from_content(mod_id);
+
         }
+        console.log("");
+        console.log("Nach der Schleife: ");
+        console.log("content: " + content);
+        console.log("blocked: " + blocked);
+        console.log("catalog_array: " + catalog_array);
 
-        index = catalog_array.indexOf(cat_id);
-        if (index != -1) {
-            catalog_array.splice(index, 1);
-        }
-
-        /*
-        index = content[sem_id - 1][i].indexOf(mod_id);
-        if (index != -1) {
-            content[sem_id - 1].splice(index, 1);
-            content[sem_id - 1][i].splice(index, 12);
-            content[sem_id - 1] = newArray;
-        }
-        */
-        remove_module_from_content(mod_id);
-        update_master_ects(mod_id);
-
-        //FEHLER
-        //Springt aus Schleife nach Aufruf
-        //Content wird nicht zurückgesetzt
-        //remove_module_from_content(mod_id);
-
+        update_rules();
     }
-    console.log("");
-    console.log("Nach der Schleife: ");
-    console.log("content: " + content);
-    console.log("blocked: " + blocked);
-    console.log("catalog_array: " + catalog_array);
-
-    update_rules();
 }
