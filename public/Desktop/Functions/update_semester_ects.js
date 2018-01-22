103f149f56a9527a5303ace5392545950ee357d5
GutHibM <unverzogen-1@yahoo.de>
2018-01-22 20:42:13 +0100

    1

--- public/Desktop/Functions/update_semester_ects.js
+++ public/Desktop/Functions/update_semester_ects.js
@@ -1,28 +1,13 @@
-function update_semester_ects(mod_id) {
+function update_semester_ects(sem_id) {
 
-    var searchedSem = -1;
     var countSemECTS = 0;
 
-
-    for (k = 0; k < content.length; k++) {
-        
-        for (l = 0; l < content[k].length; l++) {
-            if (content[k][l][0] == mod_id) {
-                if(searchedSem != k){
-                    countSemECTS=0;
-                    searchedSem = k;
-                    for (i = 0; i < content[searchedSem].length; i++) {
-                        countSemECTS += (Number.parseInt(content[searchedSem][i][9]));
-                        
-                    }
-                    countSemECTS = Number.parseInt(countSemECTS);
-                    var idString = "sem_ects_anz"+(searchedSem+1);
-                    document.getElementById(idString).textContent = countSemECTS;
-                }
-
-            }
-        }
-
+    for (i = 0; i < content[sem_id - 1].length; i++) {
+        countSemECTS += (Number.parseInt(content[sem_id - 1][i][9]));
     }
 
+    var idString = "sem_ects_anz" + (sem_id);
+    document.getElementById(idString).textContent = countSemECTS;
 }
+
+
