a5673af3d990110aab68c2826b68fdfbcde6f898
PeBoddy <peboddy@users.noreply.github.com>
2018-01-22 18:20:00 +0100

    desktop gui -> counter minus

--- public/Desktop/Functions/counter_minus.js
+++ public/Desktop/Functions/counter_minus.js
@@ -1,42 +1,32 @@
-function counter_minus()
-{
+function counter_minus() {
     count = (count == 0 ? 0 : count++);
 
     var wert = Number(document.getElementById("count").textContent);
 
-    if (wert > 0)
-    {
+    if (wert > 0) {
         wert--;
     }
 
     document.getElementById("count").textContent = wert;
-
     modules_in_sem = $("#div_semester").children().last().children().length - 1;
 
-    for (i = 0; i < modules_in_sem; i++)
-    {
+    for (i = 0; i < modules_in_sem; i++) {
+        var mod_id = $("#div_semester").children().last().children().last().children().attr('id');
+        var id = mod_id.replace("mod_","");
 
-        var id = $("#div_semester").children().last().children().filter(":nth-child(" + 2 + ")").children().first().text().replace("ID: ", "");
-
-        var mod_id = "mod_" + id;
-
-        remove_module_from_content(id);
-        
         $("#" + mod_id).remove();
 
-        $("#" + id).parent().parent().removeClass('blocked');
-        $("#" + id).parent().parent().removeClass('blocked_wpp');
-        $("#" + id + "_WPP").parent().parent().removeClass('blocked');
-        $("#" + id + "_WPP").parent().parent().removeClass('blocked_wpp');
-        $("#" + (id).toString().replace("_WPP", "")).parent().parent().removeClass('blocked');
-        $("#" + (id).toString().replace("_WPP", "")).parent().parent().removeClass('blocked_wpp');
-
+        $("#" + id).parent().parent().children().children().removeClass('blocked');
+        $("#" + id).parent().parent().children().children().removeClass('blocked_wpp');
+        $("#" + id + "_WPP").parent().parent().children().children().removeClass('blocked');
+        $("#" + id + "_WPP").parent().parent().children().children().removeClass('blocked_wpp');
+        $("#" + (id).toString().replace("_WPP", "")).parent().parent().children().children().removeClass('blocked');
+        $("#" + (id).toString().replace("_WPP", "")).parent().parent().children().children().removeClass('blocked_wpp');
+        remove_module_from_content(mod_id);
+        update_master_ects(mod_id,0);
     }
-    
-    content.pop();
-    
-    $("#div_semester").children().last().remove();
-    update_semester_ects(mod_id);
 
+    content.pop();
+    $("#div_semester").children().last().remove();
     update_rules();
 }
