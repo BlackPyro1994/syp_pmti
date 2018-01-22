d9ff83dab70d2c705fcc3e2f29db782c3f132e44
GutHibM <unverzogen-1@yahoo.de>
2018-01-22 14:27:48 +0100

    diverses in Desktop-V. + Modulkataloge.csv)

--- public/Desktop/Listener/modal_freitext_ok.js
+++ public/Desktop/Listener/modal_freitext_ok.js
@@ -1,19 +1,26 @@
 function modal_freitext_ok(click)
 {
     var name;
-    var v, u, p, pr;
+    var v, u, p, pr, ws, sose;
     var dozent;
     var ects;
+    var istPflicht;
+    var kat_id;
     
     name = $("#input-name").val();
     v = $("#input-v").val();
     u = $("#input-u").val();
     p = $("#input-p").val();
     pr = $("#input-pr").val();
+    ws = 1;
+    sose = 1;
     dozent = $("#input-dozent").val();
     ects = $("#input-ects").val();
-    
-    append_free_module(global_mod_id, global_sem_id, name, v, u, p, pr, dozent, ects);
+    istPflicht = false;
+    kat_id = "AM";
+
+    append_free_module(global_mod_id, global_sem_id, name, v, u, p, pr, ws, sose, dozent, ects, istPflicht, kat_id);
+    //append_free_module(global_mod_id, global_sem_id, name, v, u, p, pr, dozent, ects);
     
     $('#myModal-freitext').modal('hide');
     
--- public/Desktop/Listener/modal_m07_ok.js
+++ public/Desktop/Listener/modal_m07_ok.js
@@ -1,17 +1,23 @@
 function modal_m07_ok(click)
 {
     var name;
-    var v, u, p, pr;
+    var v, u, p, pr, ws, sose;
     var dozent;
     var ects;
+    var istPflicht;
+    var kat_id;
     
     name = $("#input-name-m07").val();
     v = 2;
     u = 1;
     p = 1;
     pr = 0;
+    ws = 1;
+    sose = 1;
     dozent = $("#input-dozent-m07").val();
     ects = 5;
+    istPflicht = false;
+    kat_id = "WPP";
     
     if (global_mod_id == "M07" && name == "")
     {
@@ -21,7 +27,8 @@ function modal_m07_ok(click)
     {
         dozent = "Dozent";
     }
-    append_free_module(global_mod_id, global_sem_id, name, v, u, p, pr, dozent, ects);
+    append_free_module(global_mod_id, global_sem_id, name, v, u, p, pr, ws, sose, dozent, ects, istPflicht, kat_id);
+    //append_free_module(global_mod_id, global_sem_id, name, v, u, p, pr, dozent, ects);
     $("#" + global_mod_id).parent().parent().addClass('blocked_wpp');
     
     $('#myModal-m07').modal('hide');
