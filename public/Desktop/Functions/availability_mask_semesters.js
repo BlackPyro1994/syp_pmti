36d24d3616484536be44640659b05ea383fc3f28
Alexander <BlackPyro94@gmail.com>
2018-01-22 10:37:07 +0100

    Ging alles vorher !!! Der M labbbert scheiße

--- public/Desktop/Functions/availability_mask_semesters.js
+++ public/Desktop/Functions/availability_mask_semesters.js
@@ -1,57 +1,51 @@
-/*
-
- function availability_mask_semesters(module_id)
+function availability_mask_semesters(module_id)
 {
     if (module_id)
     {
         
-        @for(mod<-modulListe){
-        if ("@{mod(0)}" == module_id)
+        store_module_data_by_id(module_id)
+        
+        // Sommer und nicht Winter
+        if (search_result[6] && !search_result[7])
         {
+            ("SOMMER");
             
-            // Sommer und nicht Winter
-            
-            if (@{mod(6)} &&
-            !(@{mod(7)})
-        )
+            if(startSem=="sommer")
             {
-                
-                ("SOMMER");
-                
-                // ###                                 $("#div_semester").children().filter(":even").addClass('include');
-                // ###                                 $("#div_semester").children().filter(":odd").removeClass('include');
-                
+                // $("#div_semester").children().filter(":even").addClass('include');
+                $("#div_semester").children().filter(":odd").removeClass('include');
                 // $("#div_semester").children().children().not('.class_click_semester_row').hide(200);
-                
-                // ###                                 $("#div_semester").children().children().not('.class_click_semester_row').css('display', 'none');
-                
+                // $("#div_semester").children().children().not('.class_click_semester_row').css('display', 'none');
             }
-            
-            // Winter und nicht Sommer
-        
-        else
-            if ((!@{mod(6)}) && @{mod(7)})
+            else if(startSem=="winter")
             {
-                
-                ("WINTER");
-                
-                // ### $("#div_semester").children().filter(":odd").addClass('include');
-                // ### $("#div_semester").children().filter(":even").removeClass('include');
-                // ### $("#div_semester").children().children().not('.class_click_semester_row').hide(200);
-                
-                // $("#div_semester").children().children().not('.class_click_semester_row').css('display', 'block');
-                
+                $("#div_semester").children().filter(":odd").addClass("bg-)
             }
-            
-            return;
-            
         }
-    }
-    }
+        // Winter und nicht Sommer
     else
-    {
-        
-        ("ELSE");
+        if ((!@{
+            mod(6)
+        }
+    ) &&
+        @
+        {
+            mod(7)
+        }
+    )
+        {
+            ("WINTER");
+            // ### $("#div_semester").children().filter(":odd").addClass('include');
+            // ### $("#div_semester").children().filter(":even").removeClass('include');
+            // ### $("#div_semester").children().children().not('.class_click_semester_row').hide(200);
+            // $("#div_semester").children().children().not('.class_click_semester_row').css('display', 'block');
+        }
+        return;
+    }
+}
+else
+{
+    ("ELSE");
         
         // $("#div_semester").children().removeClass('include');
         
@@ -59,5 +53,3 @@
         
     }
 }
-
-*/
