196c3bf0aab022691ba5ef226f7ef659566c57cc
PeBoddy <peboddy@users.noreply.github.com>
2018-01-24 03:15:17 +0100

    mobile gui -> counter_minus() REPARIERT

--- public/Mobile/Functions/counter_minus.js
+++ public/Mobile/Functions/counter_minus.js
@@ -1,40 +1,35 @@
-function counter_minus()
-{
+function counter_minus() {
+    console.log("function counter_minus()");
     var cat_id, mod_id, index;
-    
-    if (count > 0)
-    {
+
+    if (count > 0) {
         count--;
-    
-        var anz = content[global_sem_id - 1].length;
-        
-        if (anz != 0)
-        {
-            for (i = 0; i < anz; i++)
-            {
-                if (content[global_sem_id - 1][i] != 0)
-                {
-                    mod_id = content[global_sem_id - 1][i][0];
-                    cat_id = content[global_sem_id - 1][i][11];
-                    safe_semester = $("#div_semester").children();
-                    
-                    index = blocked.indexOf(mod_id);
-                    
-                    if (index != -1)
-                    {
-                        blocked.splice(index, 1);
-                    }
-                    
-                    update_master_ects(mod_id, 0);
-                    content_html.splice(global_sem_id, 1);
-                }
+
+        //var anz = content[global_sem_id - 1].length;
+        var length = content.length;
+        var inner_length = content[length-1].length;
+
+        for (i=0;i<inner_length;i++) {
+            mod_id = content[length-1][i][0];
+            cat_id = content[length-1][i][11];
+            safe_semester = $("#div_semester").children();
+
+            index = blocked.indexOf(mod_id);
+            if (index != -1) {
+                blocked.splice(index, 1);
             }
-    
-            update_rules();
-            $("#div_semester").children().last().remove();
-            
-            content.pop();
-            content.push(new Array());
+            index = catalog_array.indexOf(cat_id);
+            if (index != -1) {
+                catalog_array.splice(index, 1);
+            }
+
+            update_master_ects(mod_id, 0);
+            content_html.splice(length, 1);
+
         }
+        update_rules();
+        $("#div_semester").children().last().remove();
+        content.pop();
+        //content.push(new Array());
     }
 }
