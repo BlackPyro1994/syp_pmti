4f56a5c498105a25acf0a7e49c9dd6656a0607d7
Alexander <BlackPyro94@gmail.com>
2018-01-24 11:57:19 +0100

    My first commit

---
+++ public/Desktop/Functions/updateModuleString.js
@@ -0,0 +1,22 @@
+function updateModuleString(mod_id)
+{
+    var BreakException = {};
+    
+    try
+    {
+        modulListe.forEach(function (entry)
+        {
+            if (mod_id == entry.split(',')[0])
+            {
+                console.log(entry);
+                search_result = entry;
+                
+                throw BreakException;
+            }
+        });
+    } catch (e)
+    {
+        if (e !== BreakException) throw e;
+    }
+    
+}
