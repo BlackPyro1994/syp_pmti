ed2808bd9c67aa49fb668f6dd7f699ebacc46df4
Alexander <BlackPyro94@gmail.com>
2018-01-27 19:50:09 +0100

    WPP M07 Abbrechen REPARIERT

--- public/Desktop/Listener/modal_freitext_abbrechen.js
+++ public/Desktop/Listener/modal_freitext_abbrechen.js
@@ -4,4 +4,6 @@ function modal_freitext_abbrechen(click)
     
     $('.container-fluid.semester-gesamt').removeClass('ausgewaehlt');
     $("#" + global_mod_id).removeClass('ausgewaehlt');
+    
+    $('#myModal-freitext').modal('hide');
 }
--- public/Desktop/Listener/modal_m07_abbrechen.js
+++ public/Desktop/Listener/modal_m07_abbrechen.js
@@ -2,6 +2,8 @@ function modal_m07_abbrechen(click)
 {
     console.log("modal_m07_abbrechen("+click+")");
     
+    $('#myModal-m07').modal('hide');
+    
     $('.container-fluid.semester-gesamt').removeClass('ausgewaehlt');
     $("#" + global_mod_id).removeClass('ausgewaehlt');
 }
