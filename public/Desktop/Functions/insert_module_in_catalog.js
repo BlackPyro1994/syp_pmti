739c09f84e30d310a33b1c21c30e3dee20ed155a
PeBoddy <peboddy@users.noreply.github.com>
2018-01-28 12:40:14 +0100

    Tooltips angepasst

--- public/Desktop/Functions/insert_module_in_catalog.js
+++ public/Desktop/Functions/insert_module_in_catalog.js
@@ -1,18 +1,19 @@
-function insert_module_in_catalog(katalog_id, module_id, module_title)
-{
+function insert_module_in_catalog(katalog_id, module_id, module_title) {
     console.log("insert_module_in_catalog(" + katalog_id + "," + module_id + "," + module_title + ")");
-    
+    var tooltip;
+
     $("#" + katalog_id).parent().append('<button data-toggle="tooltip" data-placement="right" title="" ' +
         'id = ' + module_id + ' ' +
         'onClick="left_click_on_module(this)" ' +
         'class = "katalog_label rounded-catalog-module">' + module_title + '</button>');
     $("#" + module_id).hide();
-    
+
     update_module_string(module_id);
-    
+
+    tooltip = "ID: " + search_result.split(",")[0] + " V:" + search_result.split(",")[2] + " Ü:" + search_result.split(",")[3] + " P:" + search_result.split(",")[4] + " Pr:" + search_result.split(",")[5] + " Dozent: " + search_result.split(",")[8] + " ECTS: " + search_result.split(",")[9];
     $('[data-toggle="tooltip"]').tooltip({
-        title: search_result,
-        animation:true,
+        title: tooltip,
+        animation: true,
         delay: {show: 800, hide: 0}
     });
 }
